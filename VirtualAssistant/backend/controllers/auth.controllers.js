bbcf6eb962bcba93df98bc4e76158fa911ddba17
SabaAhmad04 <sabaahmad935@gmail.com>
2025-10-26 21:49:56 +0530

    Ninth Commit

--- VirtualAssistant/backend/controllers/auth.controllers.js
+++ VirtualAssistant/backend/controllers/auth.controllers.js
@@ -25,7 +25,7 @@ try {
         httpOnly:true,
        maxAge:7*24*60*60*1000,
        sameSite:"strict",
-       secure:false
+       secure: process.env.NODE_ENV === "production"
     })
 
     return res.status(201).json(user)
