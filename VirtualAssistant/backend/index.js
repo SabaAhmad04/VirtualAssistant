191532033bd7cd96699a6796b0936f27b068e500
SabaAhmad04 <sabaahmad935@gmail.com>
2025-10-26 21:16:13 +0530

    Check

--- VirtualAssistant/backend/index.js
+++ VirtualAssistant/backend/index.js
@@ -13,8 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    // origin: "https://virtual-assistant-9y56.vercel.app",
-    origin:"http://localhost:5173", // change to your frontend URL when deployed
+    origin: "https://virtual-assistant-9y56.vercel.app",
     credentials: true,
   })
 );
