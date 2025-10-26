ddbd2616fcfa34f18b00840e8aa99ed393a20dc0
SabaAhmad04 <sabaahmad935@gmail.com>
2025-10-26 20:10:24 +0530

    Eigth Commit

--- VirtualAssistant/backend/config/db.js
+++ VirtualAssistant/backend/config/db.js
@@ -1,23 +1,12 @@
-// import mongoose from "mongoose"
+import mongoose from "mongoose"
 
-// const connectDb=async ()=>{
-//     try {
-//         await mongoose.connect(process.env.MONGODB_URL)
-//         console.log("db connected")
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+const connectDb=async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("db connected")
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-// export default connectDb
-
-import mongoose from "mongoose";
-let isConnected = false;
-
-const connectDb = async () => {
-  if (isConnected) return;
-  await mongoose.connect(process.env.MONGO_URI, { /* options */ });
-  isConnected = true;
-};
-
-export default connectDb;
+export default connectDb
